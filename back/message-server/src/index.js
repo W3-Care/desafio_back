300b5283917593d0d1e0d4acc813ae64688d9986
Bernardo Lobato <bernardolobato@gmail.com>
2020-04-30 13:39:30 -0300

    creating-structure

---
+++ back/message-server/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+
+const http = require('http');
+const server = http.Server(app);
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+});
+
+server.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
