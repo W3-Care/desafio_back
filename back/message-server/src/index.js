b3005729fee05fb2301999b2539b1e7e5b6c96d5
Bernardo Lobato <bernardolobato@gmail.com>
2020-05-01 15:24:33 -0300

    adding ui to chat room

--- back/message-server/src/index.js
+++ back/message-server/src/index.js
@@ -11,13 +11,20 @@ const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
 
-  let previousId;
-  socket.join(1);
   console.log('user connected');
-   socket.on('new-message', (message) => {
-        console.log(message);
-        io.to(1).emit('new-message', message);
-      });
+  socket.on('new-message', (message) => {
+      console.log(message);
+      io.to(message.roomId).emit('new-message', message);     
+  });
+
+    socket.on('new-room', (room) => {
+      console.log('New Room ', room);
+      io.emit('new-room', room);
+    });
+    socket.on('join-room', (room) => {
+      socket.join(room);
+      console.log('Joined Room ', room);
+    });
 });
 
 server.listen(port, () => {
